c37a9ba4a0c5f45d5afc3bfb137a658a6821c45b
InTheRain <indrswara@gmail.com>
2023-08-18 22:23:59 +0700

    feat: add readme

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".create-note");
+const deleteBtn = document.querySelector(".delete-note");
 let notes = document.querySelectorAll(".input-box");
 
 
 createBtn.addEventListener("click", createNotes);
+
+
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 function createNotes()
 {
@@ -15,3 +26,38 @@ function createNotes()
     notesContainer.appendChild(inputBox).appendChild(img);
 }
 
+
+notesContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "P"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+
+});
+
+// Delete all notes
+function deleteAllNotes(){
+    notesContainer.innerHTML = "";
+    updateStorage();
+}
+
+deleteBtn.addEventListener("click", deleteAllNotes);
+
+
+//update notes
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+
+})
